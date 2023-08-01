4796f3aad9e48d67a32a728e77e9442f044eb245
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-01 21:20:17 +0530

    [Riya] Add : Pincode Validation

--- Pincode/Program.cs
+++ Pincode/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace Pincode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidatePincode validatePincode = new ValidatePincode();
+            validatePincode.Validate("400088");
+        }
+    }
+}
